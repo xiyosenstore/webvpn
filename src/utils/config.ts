19f80ed4f03929bf0671892c81d2837a070c4ad0
xiyosenstore <ziyosenstore@gmail.com>
2026-03-13 10:01:21 +0700

    Update proxy list URL and web name in config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -9,8 +9,8 @@ export const MAIN_DOMAINS = [
 export const BUG_LIST = ["dev.appsflyer.com","go.appsflyer.com","cdn.customlinks.appsflyer.com","add.customlinks.appsflyer.com","ava.game.naver.com","df.game.naver.com","quiz.int.vidio.com","quiz.staging.vidio.com","nontontv.vidio.com","quiz.vidio.com","img.email1.vidio.com","img.email2.vidio.com","img.email3.vidio.com","support.zoom.us","source.zoom.us","zoomgov.com","zoomgov","blog.webex.com","edu.ruangguru.com","gw.ruangguru.com","bimbel.ruangguru.com","blog.ruangguru.com","roboguru.ruangguru.com","io.ruangguru.com","open.spotify.com","investors.spotify.com","investor.fb.com","cache.netflix.com","npca.netflix.com","creativeservices.netflix.com","help.viu.com","www.udemy.com","info.udemy.com","teaching.udemy.com","zaintest.vuclip.com","space.byu.id","grabacademyportal.grab.com","grabalumni.grab.com","app-stg.gopay.co.id","app.gopay.co.id","hurricane.lipcon.com","www.lipcon.com","investor.medallia.com","go.medallia.com","app.midtrans.com","www.midtrans.com","api.midtrans.com","dashboard.midtrans.com","account-gopay.midtrans.com","s.shopee.co.id","cf.shopee.co.id","s.lazada.co.id","store.linefriends.com","collection.linefriends.com","help.line.me","api24-normal.tiktokv.com","api24-normal-useast1a.tiktokv.com","api24-normal-alisg.tiktokv.com","graph.instagram.com","cf.shopee.co.id.sea-sw.swiftserve.com","web.poe.garena.com","www.freefiremobile.com","dl.cvs.freefiremobile.com"];
 
 export const CONFIG = {
-  proxyListUrl: "https://raw.githubusercontent.com/bexcdx/proxyip/refs/heads/main/proxyip.json",
+  proxyListUrl: "https://github.com/xiyosenstore/ProxyList/blob/main/rawProxyList.txt",
   apiCheckUrl: "https://proxyip-check.bexcodex.xyz/",
   pathTemplate: "/{ip}-{port}",
-  webName: "BexCodex"
+  webName: "Benx-project"
 };
